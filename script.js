3ab90f42d1e550063d446a71e00b92a33890b490
harshkumargaur <gaurharsh839@gmail.com>
2022-05-05 22:53:11 +0530

    initial

---
+++ script.js
@@ -0,0 +1,110 @@
+'use strict';
+var sound1 = new Howl({
+  src: ['sounds/bubbles.mp3']
+});
+var sound2 = new Howl({
+  src: ['sounds/clay.mp3']
+});
+var sound3 = new Howl({
+  src: ['sounds/confetti.mp3']
+});
+var sound4 = new Howl({
+  src: ['sounds/corona.mp3']
+});
+var sound5 = new Howl({
+  src: ['sounds/dotted-spiral.mp3']
+});
+var sound6 = new Howl({
+  src: ['sounds/flash-1.mp3']
+});
+var sound7 = new Howl({
+  src: ['sounds/flash-2.mp3']
+});
+var sound8 = new Howl({
+  src: ['sounds/flash-3.mp3']
+});
+var sound9 = new Howl({
+  src: ['sounds/glimmer.mp3']
+});
+var sound10 = new Howl({
+  src: ['sounds/moon.mp3']
+});
+var sound11 = new Howl({
+  src: ['sounds/pinwheel.mp3']
+});
+var sound12 = new Howl({
+  src: ['sounds/piston-1.mp3']
+});
+var sound13 = new Howl({
+  src: ['sounds/piston-2.mp3']
+});
+var sound14= new Howl({
+  src: ['sounds/piston-3.mp3']
+});
+var sound15 = new Howl({
+  src: ['sounds/prism-1.mp3']
+});
+var sound16 = new Howl({
+  src: ['sounds/prism-2.mp3']
+});
+var sound17 = new Howl({
+  src: ['sounds/prism-3.mp3']
+});
+var sound18 = new Howl({
+  src: ['sounds/splits.mp3']
+});
+var sound19 = new Howl({
+  src: ['sounds/squiggle.mp3']
+});
+var sound20 = new Howl({
+  src: ['sounds/strike.mp3']
+});
+var sound21 = new Howl({
+  src: ['sounds/suspension.mp3']
+});
+var sound22 = new Howl({
+  src: ['sounds/timer.mp3']
+});
+var sound23 = new Howl({
+  src: ['sounds/ufo.mp3']
+});
+var sound24 = new Howl({
+  src: ['sounds/veil.mp3']
+});
+var sound25 = new Howl({
+  src: ['sounds/wipe.mp3']
+});
+var sound26 = new Howl({
+  src: ['sounds/zig-zag.mp3']
+});
+
+
+document.documentElement.addEventListener('keypress',function(e){
+if (e.key === 'a') { sound1.play(); }
+if (e.key === 'b') { sound2.play(); }
+if (e.key === 'c') { sound3.play(); }
+if (e.key === 'd') { sound4.play(); }
+if (e.key === 'e') { sound5.play(); }
+if (e.key === 'f') { sound6.play(); }
+if (e.key === 'g') { sound7.play(); }
+if (e.key === 'h') { sound8.play(); }
+if (e.key === 'i') { sound9.play(); }
+if (e.key === 'j') { sound10.play(); }
+if (e.key === 'k') { sound11.play(); }
+if (e.key === 'l') { sound12.play(); }
+if (e.key === 'm') { sound13.play(); }
+if (e.key === 'n') { sound14.play(); }
+if (e.key === 'o') { sound15.play(); }
+if (e.key === 'p') { sound16.play(); }
+if (e.key === 'q') { sound17.play(); }
+if (e.key === 'r') { sound18.play(); }
+if (e.key === 's') { sound19.play(); }
+if (e.key === 't') { sound20.play(); }
+if (e.key === 'u') { sound21.play(); }
+if (e.key === 'v') { sound22.play(); }
+if (e.key === 'w') { sound23.play(); }
+if (e.key === 'x') { sound24.play(); }
+if (e.key === 'y') { sound25.play(); }
+if (e.key === 'z') { sound26.play(); }
+
+})
